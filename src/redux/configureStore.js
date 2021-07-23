610552f7528bc870a33e2bb19e997841b0423126
Claramount Diego <diego.claramount@alpiq.com>
2021-07-23 10:30:42 +0200

    splitting reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, InitState} from './reducer.js';
+import { createStore, combineReducers }     from        'redux';
+import { Dishes }                           from        './dishes.js';
+import { Comments }                         from './comments.js';
+import { Promotions }                       from           './promotions';
+import { Leaders }                          from './leaders.js';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, InitState
-    );
+        combineReducers({
+            dishes:Dishes,
+            comments:Comments, 
+            promotions: Promotions, 
+            leaders: Leaders
+        }
+        )
 
+    );
     return store;
 }
